c8a938c39f4f02d75343135e167dbecc6a51cfb8
vishnukanth T <t.vishnukanth@gmail.com>
2025-07-16 22:42:09 +0530

    created separate module for angular material components..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MyMaterialModule } from './my-material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MyMaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
