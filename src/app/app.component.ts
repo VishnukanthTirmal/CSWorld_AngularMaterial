3d53ef5b8b48034972221471889da1b7110fcb65
vishnukanth T <t.vishnukanth@gmail.com>
2025-07-20 16:03:53 +0530

    routing and home component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { MyMaterialModule } from './my-material.module';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MyMaterialModule],
+  imports: [CommonModule, RouterOutlet, MyMaterialModule, RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
