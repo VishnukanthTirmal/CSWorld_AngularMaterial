169805f71b8244cf1dc3591d0e811e99e5ab2477
vishnukanth T <t.vishnukanth@gmail.com>
2025-07-22 22:43:39 +0530

    about us component added..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent,
+    },
+    {
+        path: 'aboutus',
+        component: AboutusComponent,
     }
 ];
